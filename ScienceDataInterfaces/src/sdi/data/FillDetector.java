b025977b796d8db28a59674e5ccb4ce2ae18c78c
jbf <jbf@spot5>
2015-03-13 06:47:39 -0500

    javadoc

--- ScienceDataInterfaces/src/sdi/data/FillDetector.java
+++ ScienceDataInterfaces/src/sdi/data/FillDetector.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sdi.data;
 
 /**
@@ -10,7 +5,6 @@ package sdi.data;
  * @author jbf
  * @see XYData#getFillDetector() 
  */
-
 public interface FillDetector {
     /**
      * return true if the value at the index is fill and cannot be used.
--- ScienceDataInterfaces/src/sdi/data/MultiTable.java
+++ ScienceDataInterfaces/src/sdi/data/MultiTable.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sdi.data;
 
 /**
  * This is the closest to the old Das2 TableDataSet model, where it's a 
- * set of 2-index tables as the mode changes.
+ * set of 2-index tables as the mode changes.  
+ * 
+ * Semantic rules:<ul>
+ * <li>Each table must have the same metadata.  
+ * </ul>
  * @author jbf
  */
 public interface MultiTable extends SimpleMultiTable {
