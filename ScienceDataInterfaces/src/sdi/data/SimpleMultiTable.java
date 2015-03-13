42590fcde2aea48fe31dccc68c2ee0a9f7c5b3c3
jbf <jbf@spot5>
2015-03-13 06:40:03 -0500

    experiment with multi-table scheme used at the U. Iowa RPW Group.

---
+++ ScienceDataInterfaces/src/sdi/data/SimpleMultiTable.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdi.data;
+
+/**
+ * This is the closest to the old TableDataSet model, where it's a 
+ * set of 2-index tables as the mode changes.
+ * @author jbf
+ */
+public interface SimpleMultiTable {
+  
+    int tableCount();
+    
+    SimpleBinnedData2D getTable(int i);
+  
+}
