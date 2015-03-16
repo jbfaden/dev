c356eeeaf40bd4358532d2c034d8269c44d71b0a
jbf <jbf@spot5>
2015-03-16 10:55:11 -0500

    javadoc for UncertaintyProvider

--- ScienceDataInterfaces/src/sdi/data/UncertaintyProvider.java
+++ ScienceDataInterfaces/src/sdi/data/UncertaintyProvider.java
@@ -1,10 +1,23 @@
 package sdi.data;
 
 /**
- *
+ * Provides the uncertainty of a measurement, providing the boundaries
+ * of the one-sigma error bar in the data units.
+ * 
  * @author jbf
  */
 public interface UncertaintyProvider {
-   double getUncertPlus(int i);
-   double getUncertMinus(int i);  
+    /**
+     * returns the upper bound of the error bar in the data units.
+     * @param i
+     * @return the upper bound of the error bar.
+     */
+    double getUncertPlus(int i);
+    
+    /**
+     * returns the lower bound of the error bar in the data units.
+     * @param i
+     * @return  the lower bound of the error bar.
+     */
+    double getUncertMinus(int i);  
 }
