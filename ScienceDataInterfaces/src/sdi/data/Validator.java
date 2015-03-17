fc0a7b70ffeca76e6b6ffab19ff7b3009d1c29d2
jbf <jbf@spot5>
2015-03-17 13:08:46 -0500

    finishing off javadoc

--- ScienceDataInterfaces/src/sdi/data/Validator.java
+++ ScienceDataInterfaces/src/sdi/data/Validator.java
@@ -2,58 +2,71 @@
 package sdi.data;
 
 /**
- *
+ * container for methods that check validity, when semantic checking is
+ * needed.
  * @author faden@cottagesystems.com
  */
 public class Validator {
 
+    private Validator() {
+    }
+    
     /**
      * checks for:<ol>
      * <li> monotonicity
      * <li> the x bin reference is within [low of bin i, lo of bin i+1],
-     * <li> no repeat ref. values
+     * <li> no repeat reference values
      * </ol>
      *
-     * could write one of these that traps exceptions and return true or false
-     *
      * @param d the SimpleContiguousBinnedData1D
+     * @throws IllegalArgumentException when the are semantic errors with the DataSet. 
      */
     public static void checkValid(SimpleContiguousBinnedData1D d) {
         // First check for monotonicity of bin boundaries:
         int n = d.size();
         for (int i = 0; i < n - 1; i++) {
             if (d.getXBinLo(i) >= d.getXBinLo(i + 1)) {
-                throw new RuntimeException("bad bin");
+                throw new IllegalArgumentException("bad bin");
             }
         }
         if (d.getXBinLo(n - 1) >= d.getLastXBinHi()) {
-            throw new RuntimeException("bad last bin");
+            throw new IllegalArgumentException("bad last bin");
         }
 
         // now check that refs are inside the right range
         for (int i = 0; i < n - 1; i++) {
             double r = d.getXBinReference(i);
             if (r == d.getXBinReference(i + 1)) {
-                throw new RuntimeException("duplicate ref");
+                throw new IllegalArgumentException("duplicate ref");
             }
             if ((r < d.getXBinLo(i)) || (r > d.getXBinLo(i + 1))) {
-                throw new RuntimeException("bad ref");
+                throw new IllegalArgumentException("bad ref");
             }
         }
 
         {
             double r = d.getXBinReference(n - 1);
             if ((r < d.getXBinLo(n - 1)) || (r > d.getLastXBinHi())) {
-                throw new RuntimeException("bad last ref");
+                throw new IllegalArgumentException("bad last ref");
             }
         }
     }
 
+    /**
+     * This checks for valid 
+     * SimpleContiguousBinnedData1D, and also verifies that it can getFillDetector.
+     * @param d the ContiguousBinnedData1D.
+     */
     public static void checkValid(ContiguousBinnedData1D d) {
         checkValid((SimpleContiguousBinnedData1D) d);
         d.getFillDetector(); // do something trivial
     }
-
+    
+    /**
+     * return true if the data is valid.  
+     * @param d the ContiguousBinnedData1D.
+     * @return true if the data is valid.
+     */
     public static boolean isValid(ContiguousBinnedData1D d) {
         try {
             checkValid(d);
