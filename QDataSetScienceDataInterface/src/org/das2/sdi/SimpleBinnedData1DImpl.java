9bf76cbb3dabe1a3e17651410d99d7145747cde3
jbf <jbf@spot5>
2015-03-21 10:46:02 -0500

    SimpleBinnedData1DImpl implemented.

--- QDataSetScienceDataInterface/src/org/das2/sdi/SimpleBinnedData1DImpl.java
+++ QDataSetScienceDataInterface/src/org/das2/sdi/SimpleBinnedData1DImpl.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.das2.sdi;
+
+import org.virbo.dataset.QDataSet;
+import org.virbo.dataset.SemanticOps;
+import sdi.data.Bin;
+import sdi.data.SimpleBinnedData1D;
 
 /**
  *
- * @author jbf
+ * @author faden@cottagesystems.com
  */
-public class SimpleBinnedData1DImpl {
+public class SimpleBinnedData1DImpl implements SimpleBinnedData1D {
+
+    QDataSet y;
+    QDataSet x;
+    
+    public SimpleBinnedData1DImpl( QDataSet source ) {
+        this.y= source;
+        this.x= SemanticOps.xtagsDataSet(source);
+    }
+    
+    @Override
+    public int size() {
+        return x.length();
+    }
+
+    @Override
+    public Bin getXBin(int i) {
+        return new BinImpl( x, i );
+    }
+
+    @Override
+    public double getY(int i) {
+        return y.value(0);
+    }
     
 }
