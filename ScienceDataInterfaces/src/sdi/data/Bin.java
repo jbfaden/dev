84eae3b9bdf8281791570c1026fd9d16eb7e6a7e
jbf <jbf@spot5>
2015-03-21 08:39:00 -0500

    remove BinImpl from Bin.  It is not used, and I believe it was just introduced to test the interface.  The preconditions check is moved to Validator.

--- ScienceDataInterfaces/src/sdi/data/Bin.java
+++ ScienceDataInterfaces/src/sdi/data/Bin.java
@@ -5,6 +5,7 @@ package sdi.data;
  * within the interval. When a bin cannot be used, for example if data located
  * by the Bin is to be interpolated, then the reference value should be used.
  *
+ * @see Validator#checkValid(sdi.data.Bin) 
  * @author faden@cottagesystems.com
  */
 public interface Bin {
