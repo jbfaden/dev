2a784b9a64197787f5074794af234c497a0ebdec
jbf <jbf@spot5>
2015-03-21 08:37:37 -0500

    remove BinImpl from Bin.  It is not used, and I believe it was just introduced to test the interface.  The preconditions check is moved to Validator.

--- ScienceDataInterfaces/src/sdi/data/Bin.java
+++ ScienceDataInterfaces/src/sdi/data/Bin.java
@@ -1,7 +1,5 @@
 package sdi.data;
 
-import com.google.common.base.Preconditions;
-
 /**
  * interval containing a reference location. The reference must be somewhere
  * within the interval. When a bin cannot be used, for example if data located
@@ -32,41 +30,4 @@ public interface Bin {
      * @return a single reference value for the bin
      */
     double getReference();
-}
-
-/**
- * is this a reference implementation?
- */
-class BinImpl implements Bin {
-
-    private final double min;
-    private final double max;
-    /**
-     * this is the "center" of the bin; note that min <= reference <= max
-     */
-    private final double reference;
-
-    public BinImpl(double min, double max, double reference) {
-        // TODO: clean up preconditions check
-        Preconditions.checkArgument(min <= reference, "min %s must be less than ref %s", min, reference);
-        Preconditions.checkArgument(max >= reference, "blah blah");
-        this.min = min;
-        this.max = max;
-        this.reference = reference;
-    }
-
-    @Override
-    public double getMin() {
-        return min;
-    }
-
-    @Override
-    public double getMax() {
-        return max;
-    }
-
-    @Override
-    public double getReference() {
-        return reference;
-    }
 }
