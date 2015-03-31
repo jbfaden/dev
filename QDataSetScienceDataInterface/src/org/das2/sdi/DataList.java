60d04943c61d7cba0184cba81f77e217bd200de8
jbf <jbf@spot5>
2015-03-31 09:20:20 -0500

    move over DataList stuff from SDI until this is resolved.

---
+++ QDataSetScienceDataInterface/src/org/das2/sdi/DataList.java
@@ -0,0 +1,26 @@
+
+package org.das2.sdi;
+
+/**
+ * When interfaces need a list of datasets, this interface should
+ * be used to provide a read-only container for them.
+ * 
+ * @author faden@cottagesystems.com
+ * @param <T> the element type, such as BinnedData2D
+ */
+public interface DataList<T> extends Iterable<T> {
+    
+    /**
+     * return the number of elements
+     * @return the number of elements
+     */
+    public int size();
+    
+    /**
+     * return the element at the index.
+     * @param i the index
+     * @return the element.
+     */
+    public T get( int i );
+    
+}
