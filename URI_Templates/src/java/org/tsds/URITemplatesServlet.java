126bd654cfd23e4e5b0379c3738d81eff6b887fc
jbf <faden@cottagesystems.com>
2015-10-22 06:29:57 -0500

    refactor to extract servlet stuff from business code.  Bugfix where the constant parts were not tested for equality.

--- URI_Templates/src/java/org/tsds/URITemplatesServlet.java
+++ URI_Templates/src/java/org/tsds/URITemplatesServlet.java
@@ -21,7 +21,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
+ * Servlet for creating URIs to match the spec.  This will generate
+ * as many of the directories as possible, and list when $v (versions), 
+ * $x (ignore), and the sparse modifier is found.
  * @author jbf
  */
 public class URITemplatesServlet extends HttpServlet {
@@ -59,13 +61,10 @@ public class URITemplatesServlet extends HttpServlet {
         }
         
         URL rootUrl= new URL(root);
-        InputStream in = rootUrl.openStream();
-                
         URL[] result;
-        try {
+        
+        try (InputStream in = rootUrl.openStream()) {
             result= HtmlUtil.getDirectoryListing( rootUrl, in, true );
-        } finally {
-            in.close();
         }
         
         String[] names= new String[result.length];
@@ -109,6 +108,134 @@ public class URITemplatesServlet extends HttpServlet {
         return count;
         
     }
+            
+    /**
+     * format the result to html, sent to the PrintWriter.
+     * @param uri the template, such as http://emfisis.physics.uiowa.edu/Flight/RBSP-A/L3/$Y/$m/$d/rbsp-a_magnetometer_1sec-gse_emfisis-L3_$Y$m$d_v$(v,sep).cdf
+     * @param timerange the range constraint, such as 2012-10-01/P2D
+     * @param out the output stream, which should not be closed.
+     */
+    protected void formatHtml( String uri, String timerange, PrintWriter out ) {
+            
+        if ( (timerange.split(" ") ).length==10 ) {  // make it easier to use Jon V.'s document "uri_template_test_cases.txt"
+            String[] trs= timerange.split(" ");
+            timerange= String.format( "%s-%s-%sT%s:%s/%s-%s-%sT%s:%s", (Object[]) trs );
+        }
+
+        long t0= System.currentTimeMillis();
+
+        TimeStruct[] drtr;
+        try {
+            drtr= TimeUtil.parseISO8601Range(timerange);
+        } catch ( ParseException ex ) {
+            throw new RuntimeException( "unable to parse ISO8601 timerange",ex );
+        }
+        if ( drtr==null ) throw new IllegalArgumentException("unable to interpret ISO8601 timerange: "+timerange);
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>URI Templates</title>");            
+        out.println("</head>");
+        out.println("<body>");
+
+        String[] ss= uri.split("/");
+
+        StringBuilder generateUriBuilder= new StringBuilder(ss[0]);
+
+        int i=1;
+        while ( i<ss.length && supportsGenerate(ss[i]) ) {
+            generateUriBuilder.append("/").append(ss[i]);
+            i++;
+        }
+
+        String generateUri= generateUriBuilder.toString();
+
+        String parseUri;
+        if ( generateUri.length()==uri.length() ) {
+            parseUri= "";
+        } else {
+            parseUri= uri.substring(generateUri.length()+1);
+        }
+
+        TimeParserGenerator tp= TimeParserGenerator.create(generateUri);
+
+        int i1= generateUri.indexOf("$(enum;");
+
+        String[] enums;
+        String id;
+
+        if ( i1>-1 ) {
+            int ix= generateUri.indexOf("$(enum",i1+6);
+            if (ix>-1 ) {
+                throw new IllegalArgumentException( "Template can only contain one $(enum)." );
+            } else {
+                TimeParserGenerator.EnumFieldHandler fh= (TimeParserGenerator.EnumFieldHandler) tp.getFieldHandlerByCode("enum");
+                enums= fh.getValues();
+                id= fh.getId();
+            }
+        } else {
+            enums= new String[] { "" };
+            id= "";
+        }
+
+        String st= tp.format( drtr[0], null, Collections.singletonMap( id, enums[0] ) );
+        TimeStruct[] dr;
+        try {
+            dr = tp.parse( st,null ).getTimeRange();
+        } catch (ParseException ex) {
+            throw new RuntimeException("unable to parse timerange",ex);
+        }
+
+        out.printf( "<h1>Hybrid generate/parse result</h1>");
+        out.printf( "<h3>%s</h3>\n", uri );
+        out.printf( "<p>search limited to %s</p>\n", timerange );
+
+        if ( parseUri.length()==0 ) {
+            out.printf( "<p>generation used for entire URI</p>\n" );
+        } else {
+            out.printf( "<p>generation used for " + generateUri + ", parsing for " +parseUri + " </p>\n" );
+        }
+        out.printf( "<table border=1>\n" );
+        out.printf( "<tr><td>Filename</td><td>Time Range</td><td>Version</td></tr>" );
+
+        int count= 0;
+
+        TimeStruct stop= drtr[1];
+        while ( count<=10000 && dr[0].lt( stop ) ) {
+            for ( String enum1 : enums ) {
+                st= tp.format( dr[0], dr[1], Collections.singletonMap( id, enum1 ) ); 
+
+                if ( parseUri.length()==0 ) {
+                    out.printf(  "<tr><td>"+st + "</td><td>"+ TimeUtil.formatISO8601Range(dr) + "</td><td>N/A</td><tr>\n" );
+                    count++;
+                } else {
+                    try {
+                        count+= doParse(dr, st, parseUri, out );
+                    } catch ( IOException ex ) {
+                        logger.fine("exception thrown, presumably because the folder does not exist.");
+                    }
+                }
+
+                if ( count>10000 ) {
+                    out.printf( "<tr><td></td><td>Search limited to 10000 results.</td></tr>" );
+                    break;
+                }
+                dr= TimeStruct.next( dr );
+
+            }
+        }
+
+        out.println( "</table>");
+
+        long dt= System.currentTimeMillis() - t0;
+
+        out.printf("%d results calculated in %d milliseconds.\n",count,dt);
+
+        out.println("</body>");
+        out.println("</html>");
+
+    }
     
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -121,130 +248,15 @@ public class URITemplatesServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         response.setContentType("text/html;charset=UTF-8");
+        
         try (PrintWriter out = response.getWriter()) {
-
             String uri= request.getParameter("resourceURI");
             String timerange= request.getParameter("timerange");
-            
-            if ( (timerange.split(" ") ).length==10 ) {  // make it easier to use Jon V.'s document "uri_template_test_cases.txt"
-                String[] trs= timerange.split(" ");
-                timerange= String.format( "%s-%s-%sT%s:%s/%s-%s-%sT%s:%s", (Object[]) trs );
-            }
-            
-            long t0= System.currentTimeMillis();
-            
-            TimeStruct[] drtr;
-            try {
-                drtr= TimeUtil.parseISO8601Range(timerange);
-            } catch ( ParseException ex ) {
-                throw new RuntimeException( "unable to parse ISO8601 timerange",ex );
-            }
-            if ( drtr==null ) throw new IllegalArgumentException("unable to interpret ISO8601 timerange: "+timerange);
-
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>URI Templates</title>");            
-            out.println("</head>");
-            out.println("<body>");
-                        
-            String[] ss= uri.split("/");
-            
-            StringBuilder generateUriBuilder= new StringBuilder(ss[0]);
-            
-            int i=1;
-            while ( i<ss.length && supportsGenerate(ss[i]) ) {
-                generateUriBuilder.append("/").append(ss[i]);
-                i++;
-            }
-                    
-            String generateUri= generateUriBuilder.toString();
-            
-            String parseUri;
-            if ( generateUri.length()==uri.length() ) {
-                parseUri= "";
-            } else {
-                parseUri= uri.substring(generateUri.length()+1);
-            }
-                        
-            TimeParserGenerator tp= TimeParserGenerator.create(generateUri);
-            
-            int i1= generateUri.indexOf("$(enum;");
-            
-            String[] enums;
-            String id;
-            
-            if ( i1>-1 ) {
-                int ix= generateUri.indexOf("$(enum",i1+6);
-                if (ix>-1 ) {
-                    throw new IllegalArgumentException( "Template can only contain one $(enum)." );
-                } else {
-                    TimeParserGenerator.EnumFieldHandler fh= (TimeParserGenerator.EnumFieldHandler) tp.getFieldHandlerByCode("enum");
-                    enums= fh.getValues();
-                    id= fh.getId();
-                }
-            } else {
-                enums= new String[] { "" };
-                id= "";
-            }
-            
-            String st= tp.format( drtr[0], null, Collections.singletonMap( id, enums[0] ) );
-            TimeStruct[] dr;
-            try {
-                dr = tp.parse( st,null ).getTimeRange();
-            } catch (ParseException ex) {
-                throw new RuntimeException("unable to parse timerange",ex);
-            }
-
-            out.printf( "<h1>Hybrid generate/parse result</h1>");
-            out.printf( "<h3>%s</h3>\n", uri );
-            out.printf( "<p>search limited to %s</p>\n", timerange );
-
-            if ( parseUri.length()==0 ) {
-                out.printf( "<p>generation used for entire URI</p>\n" );
-            } else {
-                out.printf( "<p>generation used for " + generateUri + ", parsing for " +parseUri + " </p>\n" );
-            }
-            out.printf( "<table border=1>\n" );
-            out.printf( "<tr><td>Filename</td><td>Time Range</td><td>Version</td></tr>" );
-
-            int count= 0;
-            
-            TimeStruct stop= drtr[1];
-            while ( count<=10000 && dr[0].lt( stop ) ) {
-                for ( String enum1 : enums ) {
-                    st= tp.format( dr[0], dr[1], Collections.singletonMap( id, enum1 ) ); 
-                    
-                    if ( parseUri.length()==0 ) {
-                        out.printf(  "<tr><td>"+st + "</td><td>"+ TimeUtil.formatISO8601Range(dr) + "</td><td>N/A</td><tr>\n" );
-                        count++;
-                    } else {
-                        try {
-                            count+= doParse(dr, st, parseUri, out );
-                        } catch ( IOException ex ) {
-                            logger.fine("exception thrown, presumably because the folder does not exist.");
-                        }
-                    }
-
-                    if ( count>10000 ) {
-                        out.printf( "<tr><td></td><td>Search limited to 10000 results.</td></tr>" );
-                        break;
-                    }
-                    dr= TimeStruct.next( dr );
-                    
-                }
-            }
-            
-            out.println( "</table>");
-            
-            long dt= System.currentTimeMillis() - t0;
-            
-            out.printf("%d results calculated in %d milliseconds.\n",count,dt);
-            
-            out.println("</body>");
-            out.println("</html>");
+            formatHtml( uri, timerange, out );
         }
+        
     }
 
 
