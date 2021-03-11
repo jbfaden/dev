ab443d6985bc34d4835ec1eec71e0751183d814a
James Peachey <James.Peachey@jhuapl.edu>
2021-03-11 08:11:44 -0500

    Remove method to provide fill detector. This was wrong and unused.
    It was wrong because it referred to the wrong interface. The
    FillDetector interface checks for fill when given an index, but
    ScalarFunctionOfOneArg doesn't have indices, only function of
    double.

--- ScienceDataInterfaces/src/sdi/functions/ScalarFunctionOfOneArg.java
+++ ScienceDataInterfaces/src/sdi/functions/ScalarFunctionOfOneArg.java
@@ -1,23 +1,14 @@
 package sdi.functions;
 
-import java.util.Optional;
-
-import sdi.data.FillDetector;
 import sdi.data.XYMetadata;
 
 /**
- * Interface representing a univariate function of double returning double, with associated metadata and fill
- * detection.
+ * Interface representing a univariate function of double returning double, with
+ * associated metadata.
  * 
  * @author James.Peachey@jhuapl.edu
  *
  */
 public interface ScalarFunctionOfOneArg extends SimpleScalarFunctionOfOneArg, Source<XYMetadata> {
-    /**
-     * return the fill detector indicating if a value is valid or fill
-     * (non-valid measurement). This is Optional, in case all the data are valid.
-     *
-     * @return the fill detector
-     */
-    Optional<FillDetector> getFillDetector();
+
 }
