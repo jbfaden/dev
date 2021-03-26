3cf21cdb705447ede001d464d8380abf61fe3d5b
James Peachey <James.Peachey@jhuapl.edu>
2021-03-26 11:59:27 -0400

    Auto-format and clean up imports. No code change.

--- QDataSetScienceDataInterface/src/org/das2/sdi/ContiguousBinnedData1DAdapter.java
+++ QDataSetScienceDataInterface/src/org/das2/sdi/ContiguousBinnedData1DAdapter.java
@@ -10,30 +10,31 @@ import sdi.data.XYMetadata;
 
 /**
  * Adapts ContiguousBinnedData1D to QDataSet.
+ * 
  * @author faden@cottagesystems.com
  */
 public class ContiguousBinnedData1DAdapter extends SimpleContiguousBinnedData1DAdapter {
-    
-    /**
-     * Adapt the ContiguousBinnedData1D to QDataSet.  QDataSet doesn't have
-     * an explicit constraint on the bins like ContiguousBinnedData1D, so we just
-     * have to line things up.
-     * @param data the data
-     * @return a QDataSet
-     */
-    public static QDataSet adapt( ContiguousBinnedData1D data ) {
-        MutablePropertyDataSet result = SimpleContiguousBinnedData1DAdapter.adapt( data );
-        result.putProperty( QDataSet.DELTA_MINUS, Adapter.getUPAdapter( result, data.getUncertProvider(), false ) );
-        result.putProperty( QDataSet.DELTA_PLUS, Adapter.getUPAdapter( result, data.getUncertProvider(), true ) );
-        result.putProperty( QDataSet.WEIGHTS, Adapter.getFillDetector( result ) );
-        XYMetadata meta= data.getMetadata();
-        result.putProperty( QDataSet.UNITS, Units.lookupUnits( meta.getYUnits().getName() ) );
-        result.putProperty( QDataSet.LABEL, meta.getYLabel() );
-        result.putProperty( QDataSet.NAME, meta.getYName() );        
-        MutablePropertyDataSet dep0= (MutablePropertyDataSet)result.property(QDataSet.DEPEND_0);
-        dep0.putProperty( QDataSet.UNITS, Units.lookupUnits( meta.getXUnits().getName() ) );
-        dep0.putProperty( QDataSet.LABEL, meta.getXLabel() );
-        dep0.putProperty( QDataSet.NAME, meta.getXName() );        
-        return result;
-    }
+
+  /**
+   * Adapt the ContiguousBinnedData1D to QDataSet. QDataSet doesn't have an explicit constraint on the
+   * bins like ContiguousBinnedData1D, so we just have to line things up.
+   * 
+   * @param data the data
+   * @return a QDataSet
+   */
+  public static QDataSet adapt(ContiguousBinnedData1D data) {
+    MutablePropertyDataSet result = SimpleContiguousBinnedData1DAdapter.adapt(data);
+    result.putProperty(QDataSet.DELTA_MINUS, Adapter.getUPAdapter(result, data.getUncertProvider(), false));
+    result.putProperty(QDataSet.DELTA_PLUS, Adapter.getUPAdapter(result, data.getUncertProvider(), true));
+    result.putProperty(QDataSet.WEIGHTS, Adapter.getFillDetector(result));
+    XYMetadata meta = data.getMetadata();
+    result.putProperty(QDataSet.UNITS, Units.lookupUnits(meta.getYUnits().getName()));
+    result.putProperty(QDataSet.LABEL, meta.getYLabel());
+    result.putProperty(QDataSet.NAME, meta.getYName());
+    MutablePropertyDataSet dep0 = (MutablePropertyDataSet) result.property(QDataSet.DEPEND_0);
+    dep0.putProperty(QDataSet.UNITS, Units.lookupUnits(meta.getXUnits().getName()));
+    dep0.putProperty(QDataSet.LABEL, meta.getXLabel());
+    dep0.putProperty(QDataSet.NAME, meta.getXName());
+    return result;
+  }
 }
