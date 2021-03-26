a1663271f980a8bbf2c1952f0a30ce5aef7c684c
James Peachey <James.Peachey@jhuapl.edu>
2021-03-26 13:46:30 -0400

    Auto-format white space and organize imports. No code changes.

--- QDataSetScienceDataInterface/src/org/das2/sdi/BinnedData1DAdapter.java
+++ QDataSetScienceDataInterface/src/org/das2/sdi/BinnedData1DAdapter.java
@@ -10,29 +10,31 @@ import sdi.data.XYMetadata;
 
 /**
  * Adapts BinnedData1D to QDataSet
+ * 
  * @author faden@cottagesystems.com
  */
 public class BinnedData1DAdapter extends SimpleBinnedData1DAdapter {
-    
-    /**
-     * return a QDataSet for BinnedData1D
-     * @param data the BinnedData1D
-     * @return a QDataSet
-     */
-    public static QDataSet adapt( BinnedData1D data ) {
-        MutablePropertyDataSet x= getX(data);
-        MutablePropertyDataSet y= getY(data);
-        XYMetadata meta= data.getMetadata();
-        x.putProperty( QDataSet.UNITS, Units.lookupUnits( meta.getXUnits().getName() ) );
-        x.putProperty( QDataSet.LABEL, meta.getXLabel() );
-        x.putProperty( QDataSet.NAME, meta.getXName() );        
-        y.putProperty( QDataSet.UNITS, Units.lookupUnits( meta.getYUnits().getName() ) );
-        y.putProperty( QDataSet.LABEL, meta.getYLabel() );
-        y.putProperty( QDataSet.NAME, meta.getYName() );
-        y.putProperty( QDataSet.DELTA_MINUS, Adapter.getUPAdapter( y, data.getYUncertProvider(), false ) );
-        y.putProperty( QDataSet.DELTA_PLUS, Adapter.getUPAdapter( y, data.getYUncertProvider(), true ) );
-        y.putProperty( QDataSet.WEIGHTS, Adapter.getWeights( y, data.getFillDetector() ) );
-        y.putProperty( QDataSet.DEPEND_0, x );
-        return y;
-    }
+
+  /**
+   * return a QDataSet for BinnedData1D
+   * 
+   * @param data the BinnedData1D
+   * @return a QDataSet
+   */
+  public static QDataSet adapt(BinnedData1D data) {
+    MutablePropertyDataSet x = getX(data);
+    MutablePropertyDataSet y = getY(data);
+    XYMetadata meta = data.getMetadata();
+    x.putProperty(QDataSet.UNITS, Units.lookupUnits(meta.getXUnits().getName()));
+    x.putProperty(QDataSet.LABEL, meta.getXLabel());
+    x.putProperty(QDataSet.NAME, meta.getXName());
+    y.putProperty(QDataSet.UNITS, Units.lookupUnits(meta.getYUnits().getName()));
+    y.putProperty(QDataSet.LABEL, meta.getYLabel());
+    y.putProperty(QDataSet.NAME, meta.getYName());
+    y.putProperty(QDataSet.DELTA_MINUS, Adapter.getUPAdapter(y, data.getYUncertProvider(), false));
+    y.putProperty(QDataSet.DELTA_PLUS, Adapter.getUPAdapter(y, data.getYUncertProvider(), true));
+    y.putProperty(QDataSet.WEIGHTS, Adapter.getWeights(y, data.getFillDetector()));
+    y.putProperty(QDataSet.DEPEND_0, x);
+    return y;
+  }
 }
--- QDataSetScienceDataInterface/src/org/das2/sdi/XYDataAdapter.java
+++ QDataSetScienceDataInterface/src/org/das2/sdi/XYDataAdapter.java
@@ -10,33 +10,35 @@ import sdi.data.XYMetadata;
 
 /**
  * Adapts XYData to QDataSet
+ * 
  * @author faden@cottagesystems.com
  */
 public class XYDataAdapter extends SimpleXYDataAdapter {
-     
-    /**
-     * return a QDataSet for the xydata
-     * @param xydata the xydata
-     * @return a QDataSet
-     */
-    public static QDataSet adapt( XYData xydata ) {
-        MutablePropertyDataSet x= getX(xydata);
-        MutablePropertyDataSet y= getY(xydata);
-        y.putProperty( QDataSet.DEPEND_0, x );
-        XYMetadata meta= xydata.getMetadata();
-        x.putProperty( QDataSet.UNITS, Units.lookupUnits( meta.getXUnits().getName() ) );
-        x.putProperty( QDataSet.LABEL, meta.getXLabel() );
-        x.putProperty( QDataSet.NAME, meta.getXName() );
-        x.putProperty( QDataSet.DELTA_MINUS, Adapter.getUPAdapter( x, xydata.getXUncertProvider(), false ) );
-        x.putProperty( QDataSet.DELTA_PLUS, Adapter.getUPAdapter( x, xydata.getXUncertProvider(), true ) );        
-        y.putProperty( QDataSet.UNITS, Units.lookupUnits( meta.getYUnits().getName() ) );
-        y.putProperty( QDataSet.LABEL, meta.getYLabel() );
-        y.putProperty( QDataSet.NAME, meta.getYName() );
-        y.putProperty( QDataSet.DELTA_MINUS, Adapter.getUPAdapter( y, xydata.getYUncertProvider(), false ) );
-        y.putProperty( QDataSet.DELTA_PLUS, Adapter.getUPAdapter( y, xydata.getYUncertProvider(), true ) );
-        y.putProperty( QDataSet.WEIGHTS, Adapter.getWeights( y, xydata.getFillDetector() ) );
-        y.putProperty( QDataSet.TITLE, meta.getName());
-        return y;
-    }
+
+  /**
+   * return a QDataSet for the xydata
+   * 
+   * @param xydata the xydata
+   * @return a QDataSet
+   */
+  public static QDataSet adapt(XYData xydata) {
+    MutablePropertyDataSet x = getX(xydata);
+    MutablePropertyDataSet y = getY(xydata);
+    y.putProperty(QDataSet.DEPEND_0, x);
+    XYMetadata meta = xydata.getMetadata();
+    x.putProperty(QDataSet.UNITS, Units.lookupUnits(meta.getXUnits().getName()));
+    x.putProperty(QDataSet.LABEL, meta.getXLabel());
+    x.putProperty(QDataSet.NAME, meta.getXName());
+    x.putProperty(QDataSet.DELTA_MINUS, Adapter.getUPAdapter(x, xydata.getXUncertProvider(), false));
+    x.putProperty(QDataSet.DELTA_PLUS, Adapter.getUPAdapter(x, xydata.getXUncertProvider(), true));
+    y.putProperty(QDataSet.UNITS, Units.lookupUnits(meta.getYUnits().getName()));
+    y.putProperty(QDataSet.LABEL, meta.getYLabel());
+    y.putProperty(QDataSet.NAME, meta.getYName());
+    y.putProperty(QDataSet.DELTA_MINUS, Adapter.getUPAdapter(y, xydata.getYUncertProvider(), false));
+    y.putProperty(QDataSet.DELTA_PLUS, Adapter.getUPAdapter(y, xydata.getYUncertProvider(), true));
+    y.putProperty(QDataSet.WEIGHTS, Adapter.getWeights(y, xydata.getFillDetector()));
+    y.putProperty(QDataSet.TITLE, meta.getName());
+    return y;
+  }
 
 }
